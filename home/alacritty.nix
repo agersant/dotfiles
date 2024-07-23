ede10801edc27b0faf4c144d71d73bd0a15b106f
Antoine Gersant <antoine.gersant@lesforges.org>
2024-07-23 11:15:08 +0000

    Change cursor style

--- home/alacritty.nix
+++ home/alacritty.nix
@@ -3,9 +3,12 @@
     programs.alacritty = {
         enable = true;
         settings = {
-            window.padding = {
-                x = 20;
-                y = 20;
+            cursor.style = "Beam";
+            window = {
+                padding = {
+                    x = 20;
+                    y = 20;
+                };
             };
         };
     };
