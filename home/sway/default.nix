81a463220bd714c5795968defbfcad5de0f73d60
Antoine Gersant <antoine.gersant@lesforges.org>
2024-07-23 08:29:36 +0000

    Adds keybind to open firefox

--- home/sway/default.nix
+++ home/sway/default.nix
@@ -4,6 +4,9 @@
     enable = true;
     config = {
       terminal = "alacritty";
+      keybindings = lib.mkOptionDefault {
+        "Mod4+3" = "exec firefox";
+      };
     };
     extraConfig = ''
       bindsym --release Super_L exec nu ${./toggle-launcher}
