7c8f0daf2ba97096dfc5ef316e270153163e4a61
Antoine Gersant <antoine.gersant@lesforges.org>
2024-07-23 11:55:50 +0000

    Firefox preferences

--- home/firefox.nix
+++ home/firefox.nix
@@ -1,22 +1,46 @@
-{...}:{
+{...}:
+
+let lock-false = {
+  Value = false;
+  Status = "locked";
+};
+
+lock-true = {
+  Value = true;
+  Status = "locked";
+};
+
+lock-empty-string = {
+  Value = "";
+  Status = "locked";
+};
+
+in {
 
   programs.firefox = {
     enable = true;
     policies = {
-
-        ExtensionSettings = 
-            let extension = shortId: uuid: {
-                name = uuid;
-                value = {
-                    install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
-                    installation_mode = "normal_installed";
-                };
+      DisableTelemetry = true;
+      DisableFirefoxStudies = true;
+      DisablePocket = true;
+      ExtensionSettings = 
+        let extension = shortId: uuid: {
+            name = uuid;
+            value = {
+                install_url = "https://addons.mozilla.org/en-US/firefox/downloads/latest/${shortId}/latest.xpi";
+                installation_mode = "normal_installed";
             };
-            in builtins.listToAttrs [
-                (extension "ublock-origin" "uBlock0@raymondhill.net")
-                (extension "1password-x-password-manager" "{d634138d-c276-4fc8-924b-40a0ea21d284}")
-            ];
-
+        };
+        in builtins.listToAttrs [
+            (extension "ublock-origin" "uBlock0@raymondhill.net")
+            (extension "1password-x-password-manager" "{d634138d-c276-4fc8-924b-40a0ea21d284}")
+        ];
+      Preferences = {
+        "extensions.pocket.enabled" = lock-false;
+        "browser.search.suggest.enabled" = lock-false;
+        "browser.newtabpage.activity-stream.enabled" = lock-false;
+        "general.autoScroll" = lock-true;
+      };
     };
   };
 
