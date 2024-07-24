8beab475a242cdd79de2523aad2015bbea02ab0e
Antoine Gersant <antoine.gersant@lesforges.org>
2024-07-24 02:33:23 -0700

    Image previews in yazi

--- home/yazi.nix
+++ home/yazi.nix
@@ -1,8 +1,12 @@
-{...}: {
+{pkgs, ...}: {
 
     programs.yazi = {
         enable = true;
         enableNushellIntegration = true;
     };
+
+    home.packages = [
+        pkgs.ueberzugpp
+    ];
 
 }
