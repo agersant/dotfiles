9d6a17ac5c9f6e87b2746c02db7d08e30c198176
Antoine Gersant <antoine.gersant@lesforges.org>
2025-06-16 21:27:35 -0700

    Disable alt+left click dragging windows

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -3,6 +3,10 @@
   hardware.graphics.enable = true;
   services.xserver.enable = true;
   services.xserver.desktopManager.budgie.enable = true;
+  services.xserver.desktopManager.budgie.extraGSettingsOverrides = ''
+    [org.gnome.desktop.wm.preferences]
+    mouse-button-modifier="<Super>"
+  '';
   services.displayManager.autoLogin.user = "agersant";
 
   environment.budgie.excludePackages = [
