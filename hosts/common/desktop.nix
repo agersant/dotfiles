d99c068b55ab6a2f92434c23c04d3339be8057fa
Antoine Gersant <antoine.gersant@lesforges.org>
2026-02-04 23:58:11 -0800

    System update (Budgie 10.10)

--- hosts/common/desktop.nix
+++ hosts/common/desktop.nix
@@ -3,11 +3,13 @@
   hardware.graphics.enable = true;
   services.xserver.enable = true;
   services.desktopManager.budgie.enable = true;
+  services.displayManager.autoLogin.user = "agersant";
+
+  # Prevent `Alt + Left Click` from moving windows
   services.desktopManager.budgie.extraGSettingsOverrides = ''
     [org.gnome.desktop.wm.preferences]
     mouse-button-modifier="<Super>"
   '';
-  services.displayManager.autoLogin.user = "agersant";
 
   environment.budgie.excludePackages = [
     pkgs.gnome-terminal
